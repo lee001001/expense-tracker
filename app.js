2037b2b0b0393945a37c748606fc6cac1d0caabd
King LEE <a0988698776@gmail.com>
2020-09-19 20:12:14 +0800

    fetch: add handlebar setting

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const methodOverride = require('method-override')
-const port = 3000
 const app = express()
-
+const port = 3000
 require('./confing/mongoose')
-// 連線異常
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.use(methodOverride('_method'))
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 // 設定 port 3000
