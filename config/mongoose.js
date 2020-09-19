bda02f702ee99fe31ed75558c1e71e47589d9986
King LEE <a0988698776@gmail.com>
2020-09-20 03:55:42 +0800

    edit the index.hbs and seeders

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+
 const db = mongoose.connection
 db.on('error', () => {
   console.log('mongodb error!')
